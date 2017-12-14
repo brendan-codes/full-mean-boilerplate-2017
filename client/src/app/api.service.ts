658d9ff512cbad00746a02e884029bc9861cc465
Brendan Stanton <bmsenate@gmail.com>
2017-12-14 15:40:58 -0800

    Update api.service.ts

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'Rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 // ===== API SERVICE =====
 // =======================
